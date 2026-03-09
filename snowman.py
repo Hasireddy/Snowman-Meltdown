2a6c6f8425fb40a30eaba1efd2250a68728be05f
Hasireddy <malikireddy.uma@gmail.com>
2026-03-09 11:12:36 +0100

    feat:implement letter guess for secret word

--- snowman.py
+++ snowman.py
@@ -2,6 +2,36 @@ import random
 
 words = ["python", "git", "github", "snowman", "meltdown"]
 
+# Snowman ASCII Art stages
+STAGES = [
+     # Stage 0: Full snowman
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
+     """,
+     # Stage 1: Bottom part starts melting
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     """,
+     # Stage 2: Only the head remains
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     """,
+     # Stage 3: Snowman completely melted
+     """
+      ___  
+     /___\\ 
+     """
+ ]
+
 def get_random_word(words):
     """Selects a random word from a list"""
     return random.choice(words)
@@ -15,8 +45,18 @@ def play_game():
     print("Welcome to the Snowman Meltdown Game!")
     print(f"Secret word selected is: {secret_word}")
 
-    guess = input("Guess a letter: ").lower()
-    print(f"You guessed: {guess}")
+    for i in range(len(secret_word)):
+        mistakes = 0
+        guess = input("Guess a letter: ").lower()
+        if guess in secret_word:
+            continue
+        else:
+            mistakes += 1
+            print(f"You made wrong guess")
+            continue
+
+    print(f"The secret word is : {secret_word}")
+
 
 
 
