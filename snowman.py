0a895302c808d90578423a89107aefaa9e813ce7
Hasireddy <malikireddy.uma@gmail.com>
2026-03-09 10:55:17 +0100

    feat:User guess secret word function

--- snowman.py
+++ snowman.py
@@ -3,7 +3,22 @@ import random
 words = ["python", "git", "github", "snowman", "meltdown"]
 
 def get_random_word(words):
+    """Selects a random word from a list"""
     return random.choice(words)
 
 
-print(get_random_word(words))
+def play_game():
+    """Guess each letter of the secret word. For every wrong
+    guess, the snowman melts"""
+
+    secret_word = get_random_word(words)
+    print("Welcome to the Snowman Meltdown Game!")
+    print(f"Secret word selected is: {secret_word}")
+
+    guess = input("Guess a letter: ").lower()
+    print(f"You guessed: {guess}")
+
+
+
+if __name__ == "__main__":
+    play_game()
